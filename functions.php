a0a17c4b9d398c5b248816d27b9f0f8f8cf134a4
Douglas Gaspar Alves <artedga@gmail.com>
2021-10-28 21:03:39 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -35,54 +35,12 @@ function hello_elementor_admin_menu() {
     );
     
 }
-
 add_action('admin_menu', 'hello_elementor_admin_menu');
 
 if ( is_admin() ) {
 	require 'class-tgm-plugin-activation.php';
 	require 'rec.php';
 } 
-
-
-
-function hello_elementor_widgets_init() {
-		register_sidebar(
-		array(
-			'name'          => __( 'Sidebar', 'hello_elementor' ),
-			'id'            => 'sidebar-1',
-			'description'   => __( 'Add widgets here to appear in your sidebar.', 'hello_elementor' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-
-	register_sidebar(
-		array(
-			'name'          => __( 'Sidebar 2', 'hello_elementor' ),
-			'id'            => 'sidebar-2',
-			'description'   => __( 'Appears at the bottom of the content on posts and pages.', 'hello_elementor' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-
-	register_sidebar(
-		array(
-			'name'          => __( 'Sidebar 3', 'hello_elementor' ),
-			'id'            => 'sidebar-3',
-			'description'   => __( 'Appears at the bottom of the content on posts and pages.', 'hello_elementor' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'hello_elementor_widgets_init' );
 
 function hello_elementor_gutenberg_suport (){
 
@@ -93,5 +51,4 @@ function hello_elementor_gutenberg_suport (){
 	add_theme_support( 'align-wide' );
 
 }
-
 add_action( 'after_setup_theme','hello_elementor_gutenberg_suport' );
